b22cddf498c2f992e33e119dda18fd95b5765668
EduardoRDS-dev <70782900+EduardoRDS-dev@users.noreply.github.com>
2021-02-24 18:13:17 -0300

    Update InterfaceDaoTest.java

--- src/br/com/devdojo/polomorfismo/polimorfismo_interface/teste_Interface_dao/InterfaceDaoTest.java
+++ src/br/com/devdojo/polomorfismo/polimorfismo_interface/teste_Interface_dao/InterfaceDaoTest.java
@@ -2,7 +2,7 @@ package br.com.devdojo.polomorfismo.polimorfismo_interface.teste_Interface_dao;
 
 
 import br.com.devdojo.polomorfismo.polimorfismo_interface.InterfaceDAO;
-import br.com.devdojo.polomorfismo.polimorfismo_interface.SalvarEmArquivoDeTesto;
+import br.com.devdojo.polomorfismo.polimorfismo_interface.SalvarEmArquivoDeTexto;
 import br.com.devdojo.polomorfismo.polimorfismo_interface.SavarNoBancoDeDados;
 
 public class InterfaceDaoTest {
@@ -12,7 +12,7 @@ public class InterfaceDaoTest {
         InterfaceDAO salvarNoBancoDeDados = new SavarNoBancoDeDados();
         salvarNoBancoDeDados.salvarDados();
 
-        InterfaceDAO salvarNoArquivoTxt = new SalvarEmArquivoDeTesto();
+        InterfaceDAO salvarNoArquivoTxt = new SalvarEmArquivoDeTexto();
         salvarNoArquivoTxt.salvarDados();
   }
 }
