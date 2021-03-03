586bb9d31a96c7789bb9c2054ca8f825cca26f66
EduardoRDS-dev <Eduardo_Rodrigues_Santos@protonmail.ch>
2021-03-03 19:23:42 -0300

    adicionando variavel

--- src/br/com/devdojo/polomorfismo/funcionarioparagamento/Funcionario.java
+++ src/br/com/devdojo/polomorfismo/funcionarioparagamento/Funcionario.java
@@ -4,11 +4,14 @@ public abstract class Funcionario {
     protected String nome;
     protected String funcao;
     protected double salario;
+    protected String descricao;
 
     public Funcionario(String nome, String funcao, double salario) {
         this.nome = nome;
         this.funcao = funcao;
         this.salario = salario;
+
+
     }
 
     public Funcionario() {
@@ -39,4 +42,5 @@ public abstract class Funcionario {
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
 }
